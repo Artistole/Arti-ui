0efabb787d63e535f8cb9d2b99a447527ae89208
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2022-02-16 17:22:23 +0530

    feat- add responsiveness

--- toast/toast.js
+++ toast/toast.js
@@ -9,3 +9,11 @@ function toast() {
 		document.querySelector(".toast-wrapper").style.display = "none";
 	}
 }
+
+const hamburger = document.querySelector(".hamburger-container");
+
+hamburger.addEventListener("click", toggle);
+
+function toggle() {
+	document.querySelector(".side-bar").classList.toggle("hide");
+}
