2bfa9a6bf05ed69f52a71767c8610e5f2a5d2ca3
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2022-07-25 18:50:23 +0530

    change modal

--- components/modal/app.js
+++ components/modal/app.js
@@ -4,29 +4,29 @@ const modal = document.querySelector(".modal");
 
 modalInitialise.addEventListener("click", () => {
 	document.querySelector(".modal").style.display = "block";
-	document.querySelector("header").style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-	document.querySelector(".modal-initialise").style.opacity = "0.5";
-	document.querySelector("iframe").style.opacity = "0.5";
+	document.querySelector("#overlay").style.display = "block";
 	document.querySelector("#overlay").style.backgroundColor =
 		"rgba(0, 0, 0, 0.5)";
 });
 
 modalClose.addEventListener("click", () => {
 	document.querySelector(".modal").style.display = "none";
-	document.querySelector("#overlay").style.backgroundColor = "transparent";
-	document.querySelector("header").style.backgroundColor = "white";
-	document.querySelector("iframe").style.opacity = "1";
-	document.querySelector(".modal-initialise").style.opacity = "1";
+	document.querySelector("#overlay").style.display = "none";
+
+	// document.querySelector("header").style.backgroundColor = "white";
+	// document.querySelector("iframe").style.opacity = "1";
+	// document.querySelector(".modal-initialise").style.opacity = "1";
 });
 
 document.addEventListener("mouseup", function (e) {
 	var container = document.querySelector(".modal");
 	if (!container.contains(e.target)) {
 		container.style.display = "none";
-		document.querySelector("#overlay").style.backgroundColor = "transparent";
-		document.querySelector("header").style.backgroundColor = "white";
-		document.querySelector(".modal-initialise").style.opacity = "1";
-		document.querySelector("iframe").style.opacity = "1";
+		document.querySelector("#overlay").style.display = "none";
+		// document.querySelector("#overlay").style.backgroundColor = "transparent";
+		// document.querySelector("header").style.backgroundColor = "white";
+		// document.querySelector(".modal-initialise").style.opacity = "1";
+		// document.querySelector("iframe").style.opacity = "1";
 	}
 });
 
